d722470de8a884bc539daf3abb34e2a258144812
Amy Posten <amyposten85@gmail.com>
2021-09-28 13:53:31 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector(`#create-task-form`);
+  form.addEventListener(`submit`, newTask);
 });
+
+const newTask = function (event) {
+  event.preventDefault();
+  let input = event.target[`new-task-description`].value;
+  appendTask(input);
+
+  event.target[`new-task-description`].value = ``;
+};
+
+const appendTask = function (input) {
+  let ul = document.querySelector(`#tasks`);
+  let li = document.createElement(`li`);
+
+  li.innerText = input;
+
+  ul.appendChild(li);
+};
